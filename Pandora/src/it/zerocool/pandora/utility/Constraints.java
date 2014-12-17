763b34386c21ea1568fe44194ed6201e63ec51f8
Marco Battisti <marco.battisti83@gmail.com>
2014-12-17 20:21:16 +0100

    Constraints file

---
+++ Pandora/src/it/zerocool/pandora/utility/Constraints.java
@@ -0,0 +1,33 @@
+/**
+ * Project: Pandora
+ * File it.zerocool.pandora.utility/Contraints.java
+ * @author Marco Battisti
+ */
+package it.zerocool.pandora.utility;
+
+/**
+ * Class containing constraint values
+ * @author Marco Battisti
+ *
+ */
+public class Constraints {
+
+	//Empty value
+	public static final String EMPTY_VALUE = "";
+	
+	//Places types
+	public static final int TYPE_TOSEE = 1;
+	public static final int TYPE_EAT = 2;
+	public static final int TYPE_SLEEP = 3;
+	public static final int TYPE_SERVICE = 4;
+	public static final int TYPE_SHOP = 5;
+	
+	
+	/**
+	 * 
+	 */
+	private Constraints() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
